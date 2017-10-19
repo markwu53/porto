5d1017dd5903490dc5f6630db2d47b9144b9bb62
Mark Wu <mang.wu@gmail.com>
2017-10-18 23:22:22 -0400

    check fields

--- porto/explore.py
+++ porto/explore.py
@@ -2,8 +2,72 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 path = "/Users/T162880/Documents/Programs/kaggle/porto/"
+path = "/Users/apple/Documents/Programs/kaggle/porto/"
 train_file = "train.csv"
 columns = "id,target,ps_ind_01,ps_ind_02_cat,ps_ind_03,ps_ind_04_cat,ps_ind_05_cat,ps_ind_06_bin,ps_ind_07_bin,ps_ind_08_bin,ps_ind_09_bin,ps_ind_10_bin,ps_ind_11_bin,ps_ind_12_bin,ps_ind_13_bin,ps_ind_14,ps_ind_15,ps_ind_16_bin,ps_ind_17_bin,ps_ind_18_bin,ps_reg_01,ps_reg_02,ps_reg_03,ps_car_01_cat,ps_car_02_cat,ps_car_03_cat,ps_car_04_cat,ps_car_05_cat,ps_car_06_cat,ps_car_07_cat,ps_car_08_cat,ps_car_09_cat,ps_car_10_cat,ps_car_11_cat,ps_car_11,ps_car_12,ps_car_13,ps_car_14,ps_car_15,ps_calc_01,ps_calc_02,ps_calc_03,ps_calc_04,ps_calc_05,ps_calc_06,ps_calc_07,ps_calc_08,ps_calc_09,ps_calc_10,ps_calc_11,ps_calc_12,ps_calc_13,ps_calc_14,ps_calc_15_bin,ps_calc_16_bin,ps_calc_17_bin,ps_calc_18_bin,ps_calc_19_bin,ps_calc_20_bin"
+columns = columns.split(",")
+len(columns)
+"""
+id,
+target,
+ps_ind_01, cat-0-8
+ps_ind_02_cat, cat-1-4-n
+ps_ind_03, cat-0-12
+ps_ind_04_cat, cat-0-1-n
+ps_ind_05_cat, cat-0-7-n
+ps_ind_06_bin, cat-0-1
+ps_ind_07_bin, cat-0-1
+ps_ind_08_bin, cat-0-1
+ps_ind_09_bin, cat-0-1
+ps_ind_10_bin, cat-0-1
+ps_ind_11_bin, cat-0-1-all0
+ps_ind_12_bin, cat-0-1-all0
+ps_ind_13_bin, cat-0-1-all0
+ps_ind_14, cat-0-4-all0
+ps_ind_15, cat-0-14
+ps_ind_16_bin, cat-0-1
+ps_ind_17_bin, cat-0-1
+ps_ind_18_bin, cat-0-1
+ps_reg_01, float-0-1
+ps_reg_02, float-0-2
+ps_reg_03, float-0-5-n
+ps_car_01_cat, cat-0-12-n
+ps_car_02_cat, cat-0-1-n
+ps_car_03_cat, cat-0-1-nnn
+ps_car_04_cat, cat-0-10
+ps_car_05_cat, cat-0-1-nn
+ps_car_06_cat, cat-0-18
+ps_car_07_cat, cat-0-1-n
+ps_car_08_cat, cat-0-1
+ps_car_09_cat, cat-0-4
+ps_car_10_cat, cat-0-2
+ps_car_11_cat, cat-0-120
+ps_car_11, cat-0-3-n
+ps_car_12, float-0-1-n
+ps_car_13, float-0-2
+ps_car_14, float-0-1-n
+ps_car_15, float-0-4
+ps_calc_01, float-0-1
+ps_calc_02, float-0-1
+ps_calc_03, float-0-1
+ps_calc_04, cat-0-5
+ps_calc_05, cat-0-6
+ps_calc_06, int-0-10
+ps_calc_07, int-0-10
+ps_calc_08, int-0-12
+ps_calc_09, int-0-8
+ps_calc_10, int-0-25
+ps_calc_11, int-0-20
+ps_calc_12, int-0-10
+ps_calc_13, int-0-14
+ps_calc_14, int-0-25
+ps_calc_15_bin, int-0-1
+ps_calc_16_bin, int-0-1
+ps_calc_17_bin, int-0-1
+ps_calc_18_bin, int-0-1
+ps_calc_19_bin, int-0-1
+ps_calc_20_bin,  int-0-1
+"""
 
 values = []
 with open(path+train_file) as fd:
@@ -12,10 +76,11 @@ with open(path+train_file) as fd:
         line = fd.readline()
         if not line: break
         fields = line.strip().split(",")
-        values.append(fields[1])
+        values.append(fields)
 
-values = [ float(value) for value in values ]
-plt.hist(values)
+def f(n):
+    print([value[n] for value in values[:10]])
+    plt.hist([float(value[n]) for value in values])
 
 a = [ value for value in values if value < 0.1]
 b = [ value for value in values if value > 0.9]
